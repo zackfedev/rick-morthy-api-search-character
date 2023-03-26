6c76a3a3fbbab61cd347e6156eb87114cc7da466
azzakyandhika <zakiiws22@gmail.com>
2023-03-27 06:00:57 +0700

    deleting use effect for handling state

--- src/context/CharContext.jsx
+++ src/context/CharContext.jsx
@@ -1,15 +1,11 @@
 import { Pagination } from "@mui/material";
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useMemo } from "react";
 
 export const CharContext = createContext([]);
 
 export const CharProvider = ({ children }) => {
-  const [page, setPage] = useState(1);
   const [char, setChar] = useState([]);
-
-  const handlePage = (e, value) => {
-    setPage(value);
-  };
+  const [page, setPage] = useState(1);
 
   const getCharacter = async () => {
     const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`);
@@ -18,22 +14,27 @@ export const CharProvider = ({ children }) => {
     setChar(data.results);
   };
 
+  let isFetching = false;
+
   useEffect(() => {
-    getCharacter();
+    if (!isFetching) {
+      isFetching = true;
+
+      getCharacter();
+    }
   }, [page]);
 
-  const charInfo = {
-    char,
-    page,
+  const handleChangePage = (e, value) => {
+    setPage(value);
   };
 
   return (
-    <CharContext.Provider value={charInfo}>
+    <CharContext.Provider value={char}>
       <Pagination
         sx={{ pb: 3 }}
         count={10}
         page={page}
-        onChange={handlePage}
+        onChange={handleChangePage}
         size='large'
       />
       {children}
--- src/lib/useSearch.jsx
+++ src/lib/useSearch.jsx
@@ -3,10 +3,10 @@ import { CharContext } from "../context/CharContext";
 
 const useSearch = (searchTerm) => {
   const [searchResult, setSearchResult] = useState([]);
-  const { char } = useContext(CharContext);
+  const char = useContext(CharContext);
 
-  useMemo(() => {
-    const filtered = char.filter((result) =>
+  const searchFilter = useMemo(() => {
+    const filtered = char?.filter((result) =>
       result.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
